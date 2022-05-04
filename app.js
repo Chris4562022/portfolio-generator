79a3e0738c62dc6a33f6182afed9e243f582f255
Chris4562022 <williamschris83@gmail.com>
2022-05-04 13:44:41 -0400

    updating the hrml to include more than one project

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
 
 // const pageHTML = generatePage(name, github);
 
@@ -10,6 +11,8 @@ const inquirer = require('inquirer');
 
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
+
+
 
 inquirer
   const promptUser = () => {
@@ -137,9 +140,17 @@ Add a New Project
 
 
 
-  promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-    console.log(portfolioData);
-    
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Page created! Check out index.html in this directory to see it!');
   });
+});
+  
+    
+  
